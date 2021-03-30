85c7af56b334d3ba9a7e6420a0716a1e77916efd
Adam Cohill <adamcohill16@gmail.com>
2021-03-30 02:30:59 -0400

    final greeting

--- Greeting/ProgramUI.cs
+++ Greeting/ProgramUI.cs
@@ -32,15 +32,169 @@ namespace Greeting
                     //create
                     //display
                     //update
-                    //delet
+                    //deletE
                     case "1":
                         CreateNewClient();
                         break;
+                    case "2":
+                        DisplayClientList();
+                        break;
+                    case "3":
+                        UpdateExistingClient();
+                        break;
+                    case "4":
+                        DeleteClient();
+                        break;
+                    case "5":
+                        running = false;
+                        break;
+
                     default:
+                        Console.WriteLine("Please enter valid option........");
                         break;
                 }
+                Console.WriteLine("press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
             }
             
         }
-    }
+        private void CreateNewClient()
+        {
+            Client client = new Client();
+            Console.Clear();
+            
+            Console.WriteLine("Enter clientnumber here:");
+            client.ClientNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Firstname of Client here:");
+            client.FirstName = Console.ReadLine();
+            
+            Console.WriteLine("Enter Lastname of Client here:");
+            client.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter type of Client here:(Active, Pending, Complete)");
+            client.ClientType = Console.ReadLine().ToLower();
+
+            bool emailSet = false;
+            while (!emailSet)
+            {
+                if (client.ClientType == "complete")
+                {
+                    client.Email = "Hi there stranger!! Its been a while since weve seen you would you like to see our latest deals!?";
+                    emailSet = true;
+                }
+                else if (client.ClientType == "active")
+                {
+                    client.Email = "Thank you for working with our company please accept this coupon as gratitude!!";
+                    emailSet = true;
+                }
+                else if (client.ClientType == "pending")
+                {
+                    client.Email = "LEMU EMU SUCKS! COME TO KOMODO INSURANCE INSTEAD!!\n" + "DONT BE THE EMU EAT THE EMU!!!!!";
+                    emailSet = true;
+                }
+                else
+                {
+                    Console.WriteLine("ENTER VALID OPTION!!!:");
+                }
+            }
+
+            Console.WriteLine($"Your client type is : {client.ClientType}");
+            _clientRepo.AddNewClient(client);
+        }
+
+        private void DisplayClientList()
+        {
+            Console.Clear();
+            List<Client> clients = _clientRepo.GetClient();
+
+            foreach (Client client in clients)
+            {
+                Console.WriteLine($"Client number is: {client.ClientNum}\n" +
+                    $"Clients FirstName Is:{client.FirstName}\n"+
+                    $"Clients LastName Is:{client.LastName}\n" +
+                    $"Clients Type Is:{client.ClientType}\n" +
+                    $"Email to send to client is: {client.Email}");
+            }
+        }
+        private void UpdateExistingClient()
+        {
+            DisplayClientList();
+
+            Console.WriteLine("Enter the client number you would like to update.");
+            int oldClient = Convert.ToInt32(Console.ReadLine());
+
+            Client client = new Client();
+            Console.Clear();
+
+            Console.WriteLine("Enter clientnumber here:");
+            client.ClientNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Firstname of Client here:");
+            client.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Lastname of Client here:");
+            client.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter type of Client here:(Active, Pending, Complete)");
+            client.ClientType = Console.ReadLine().ToLower();
+
+            bool emailSet = false;
+            while (!emailSet)
+            {
+                if (client.ClientType == "complete")
+                {
+                    client.Email = "Hi there stranger!! Its been a while since weve seen you would you like to see our latest deals!?";
+                    emailSet = true;
+                }
+                else if (client.ClientType == "active")
+                {
+                    client.Email = "Thank you for working with our company please accept this coupon as gratitude!!";
+                    emailSet = true;
+                }
+                else if (client.ClientType == "pending")
+                {
+                    client.Email = "LEMU EMU SUCKS! COME TO KOMODO INSURANCE INSTEAD!!\n" + "DONT BE THE EMU EAT THE EMU!!!!!";
+                    emailSet = true;
+                }
+                else
+                {
+                    Console.WriteLine("ENTER VALID OPTION!!!:");
+                    Console.ReadLine();
+                }
+            }
+            Console.WriteLine($"Your client type is : {client.ClientType}");
+            bool wasUpdated = _clientRepo.UpdateExistingClient(oldClient, client);
+            if (wasUpdated)
+            {
+                Console.WriteLine("Update Successful!");
+            }
+            else
+            {
+                Console.WriteLine("YOU MESSED UP TRY HARDER!!!!!");
+            }
+        }
+
+        private void DeleteClient()
+        {
+            Console.Clear();
+
+            DisplayClientList();
+
+            Console.WriteLine("Enter the client number that you would like to delete.");
+            int responce = Convert.ToInt32(Console.ReadLine());
+            bool deleted = _clientRepo.DeleteExistingClient(responce);
+
+            if (deleted)
+            {
+                Console.WriteLine("You erased that client from total existance. I hope that makes you happy.");
+
+            }
+            else
+            {
+                Console.WriteLine("The Client is imortal and could not be deleted..... or you did something wrong.");
+            }
+        }
+     }
 }
