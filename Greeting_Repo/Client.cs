a4cdf5c6ba3debccada332207814a9719d26f443
Adam Cohill <adamcohill16@gmail.com>
2021-04-01 20:32:58 -0400

    working on tests

--- Greeting_Repo/Client.cs
+++ Greeting_Repo/Client.cs
@@ -13,6 +13,10 @@ namespace Greeting
         public string LastName { get; set; }
         public string ClientType {get; set; }
         public string Email { get; set; }
+        public string V1 { get; }
+        public string V2 { get; }
+        public string V3 { get; }
+
         public Client() { }
         public Client(int clientNum, string firstName, string lastName, string clientType, string email)
         {
@@ -21,6 +25,13 @@ namespace Greeting
             LastName = lastName;
             ClientType = clientType;
             Email = email;
+        }
+
+        public Client(string v1, string v2, string v3)
+        {
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
         }
     }
 }
